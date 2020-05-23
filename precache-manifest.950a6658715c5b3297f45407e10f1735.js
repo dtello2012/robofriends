8f708162a28d0da79cc85bf4379116d55192adb1
David Tello <dtello@seamgen.com>
2020-05-22 23:31:55 -0700

    Updates

---
+++ precache-manifest.950a6658715c5b3297f45407e10f1735.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c5540a11eb0c9ae8b7e7759f045da07",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "2e0e71c74667d7c4bc61",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "fe8046d06f580639da7d",
+    "url": "/robofriends/static/css/main.57b2a68b.chunk.css"
+  },
+  {
+    "revision": "2e0e71c74667d7c4bc61",
+    "url": "/robofriends/static/js/2.abd58c9c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.abd58c9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe8046d06f580639da7d",
+    "url": "/robofriends/static/js/main.fc73e020.chunk.js"
+  },
+  {
+    "revision": "99557a09ab68caec6407",
+    "url": "/robofriends/static/js/runtime-main.229ccf3b.js"
+  }
+]);
--- src/components/scroll/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "./index.css";
-
-export default function Scroll(props) {
-  return <div className="scroll">{props.children}</div>;
-}
